27e42c78000ac4d55c65f4ee9e4e27886ce84912
Kalelrebello <86852126+Kalelrebello@users.noreply.github.com>
2021-07-02 16:32:11 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,12 @@
+alert('Oi, esse é meu primeiro site, se achar que precisa de alguma melhoria me diga :D')
+
+let txt = "";
+
+txt += "Cookies Enabled:" + navigator.cookieEnabled + "\n";
+txt += "Browser Language:" + navigator.language + "\n";
+txt += "Browser Online:" + navigator.onLine + "\n";
+txt += "Platform:" + navigator.platform + "\n";
+
+alert(txt);
+
+console.log(txt);
